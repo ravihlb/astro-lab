a4afb09d0555ca8b6d1643c8bd8ae3c19550a67f
ravihlb <ravi@deliverydireto.com.br>
2023-01-03 19:39:03 -0300

    feat: add welcome page with nested component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import Pages from './Pages.svelte'
+    const blogName = 'Astro-Lab'
+
+    let count = 0
+    $: doubled = count * 2
+    function increment () {
+        count += 1
+    }
+</script>
+
+<style>
+    .color-purple {
+        color: #512DA8
+    }
+
+    .h-centered {
+        text-align: center
+    }
+</style>
+
+<h1 class="h-centered">
+    Welcome to
+    <br>
+    <span class="color-purple">{blogName}</span>
+</h1>
+
+<Pages/>
+
+<p>
+    Count: {count}
+
+    <button class="h.centered" on:click={increment}>
+        +1
+    </button>
+</p>
+
+<p>Count doubled: {doubled}</p>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
