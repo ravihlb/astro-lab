136ee1bdc7ec1f05a56ab068a44df2b4554e9320
ravihlb <ravihlb@protonmail.com>
2023-03-27 12:47:35 -0300

    feat(articles): add layout!

---
+++ src/routes/articles/+layout.svelte
@@ -0,0 +1,28 @@
+<style lang="sass">
+    :global(#cover)
+        height: 35em
+        width: 100%
+        object-fit: cover
+        margin-top: 2.5em
+        margin-bottom: 2em
+
+    img
+        border-radius: 1em
+        display: block
+        margin: auto
+        width: 80%
+
+    :global(h1, h2)
+        color: var(--dark-purple)
+        text-align: center
+
+    :global(h1)
+        font-size: 3em
+
+    :global(h2)
+        font-size: 2em
+
+</style>
+
+<slot />
+
