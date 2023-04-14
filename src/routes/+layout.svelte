9a76cac72882575495d73f592b41bf012bf91293
ravihlb <ravi@deliverydireto.com.br>
2023-04-14 06:01:29 -0300

    chore: update root layout and yarn.lock

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-    import Navbar from 'src/components/Navbar.svelte'
+    import Navbar from '@components/Navbar.svelte'
 </script>
 
 <style lang="sass">
@@ -10,8 +10,17 @@
         color: var(--dark-purple)
 
     :global(body)
+        font-family: sans-serif
         padding: 0 3em 3em
         margin: 0
+        font-weight: lighter
+
+    :global(.span-mobile)
+        @media screen and (max-width: 500px)
+            width: 100vw
+            margin-left: -3rem
+            padding-left: -3rem
+            padding-right: -3rem
 
     :global(.container)
         max-width: 60em
@@ -24,11 +33,25 @@
         font-size: 1.2em
         margin-right: 0px
 
-    :global(p img)
-        display: block
-        margin: 0 auto
-        height: 25em
-        max-width: 60em
+    :global(.crop)
+        width: 200px
+        height: 150px
+        overflow: hidden
+
+    :global(.crop img)
+        width: 400px
+        height: 300px
+        margin: -2em 0 0 100px
+
+    :global(h1)
+        font-size: 2em
+
+    :global(h2)
+        font-size: 1.5em
+
+    :global(h3)
+        font-size: 1.3em
+
 </style>
 
 <html lang="en">
