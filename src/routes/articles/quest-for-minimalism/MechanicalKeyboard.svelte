50310fc79329159dab79d33357fa58711089f680
ravihlb <ravi@deliverydireto.com.br>
2023-04-14 06:47:12 -0300

    wip: article

---
+++ src/routes/articles/quest-for-minimalism/MechanicalKeyboard.svelte
@@ -0,0 +1,27 @@
+<div id="video-div" class="span-mobile">
+    <iframe
+        src="https://www.youtube.com/embed/jO5IaAKTKsQ"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+        >
+    </iframe>
+</div>
+
+<style lang="sass">
+    #video-div
+        margin: auto
+        width: 50vw
+        height: 25rem
+
+    iframe
+        width: 100%
+        height: 100%
+        
+    @media screen and (max-width: 500px)
+        #video-div
+            margin: 0
+            height: 50%
+            width: 150%
+</style>
