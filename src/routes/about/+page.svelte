c38623f894d13b40f214a2a077b3bdf8cb1106f9
ravihlb <ravihlb@protonmail.com>
2024-06-25 03:54:48 -0300

    feat: `/about` page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<html lang="en" class="main">
+  <section id="section-about-me">
+
+    <p>
+      Hi! my name's Ravi
+      <br />
+      <br />
+      I am a Software QA Engineer from Brazil aspiring to become a full time [backend, frontend, software, game] developer
+      <br />
+      <br />
+      Here's where you can find my stuff:
+      <br />
+    </p>
+
+    <div id="socials">
+
+      <div id="github">
+        <a href="https://github.com/ravihlb" target="_blank">Github</a>
+      </div>
+
+      <div id="linkedin">
+        <a href="https://www.linkedin.com/in/ravihlb/" target="_blank">LinkedIn</a>
+      </div>
+    </div>
+
+  </section>
+</html>
