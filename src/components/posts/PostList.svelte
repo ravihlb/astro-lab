fa94d95d70dec481bcce3b49cf01edad42a77e22
ravihlb <ravihlb@protonmail.com>
2024-06-25 06:37:16 -0300

    refactor(PostList): remove `li` bullet points + add newline

--- src/components/posts/PostList.svelte
+++ src/components/posts/PostList.svelte
@@ -5,7 +5,17 @@
 <html lang="eng">
     <div>
         {#each posts as props}
-            <li><a href="{props.URL}">{props.title}</a> - {props.publicationDate}</li>
+            <li><a href="{props.URL}">{props.title}</a>
+                <br />
+                {props.publicationDate}
+            </li>
         {/each}
     </div>
 </html>
+
+<style lang="css">
+    li {
+        display: inline-block;
+        padding-bottom: 2.5em;
+    }
+</style>
