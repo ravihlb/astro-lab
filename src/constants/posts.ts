b77e85e47c4a7f9f7494793726d6f91bd6f7d5d2
ravihlb <ravihlb@protonmail.com>
2024-06-25 05:26:14 -0300

    feat(post): How to set which filetypes LSPs are run on in Neovim

--- src/constants/posts.ts
+++ src/constants/posts.ts
@@ -1,6 +1,18 @@
-import type { PostProperties } from '@components/shared/ImportHelper'
+export type PostProperties = {
+    title: string,
+    subtitle?: string,
+    publicationDate: string,
+    URL: string
+}
+
+import neovimFileTypesLspTitle from '@/src/routes/posts/neovim-filetypes-lsp/+page.svx'
 
 const posts: PostProperties[] = [
+    {
+        title: 'How to set which filetypes LSPs are run on in Neovim',
+        publicationDate: (new Date('2024-06-25 05:22')).toDateString(),
+        URL: 'posts/neovim-filetypes-lsp'
+    },
     {
        title: 'From Bloated Windows to Minimal Arch Linux',
        subtitle: 'A Spectrum of Operating System Bareness',
