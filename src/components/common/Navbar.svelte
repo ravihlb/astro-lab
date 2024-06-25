7e3d835f0dd813c7c6932df04d745c3594bab01f
ravihlb <ravihlb@protonmail.com>
2024-06-25 03:51:22 -0300

    fix(Navbar): 100% width

--- src/components/common/Navbar.svelte
+++ src/components/common/Navbar.svelte
@@ -6,27 +6,63 @@
 
 <style lang="sass">
     nav
-        margin: 0 0 2% 5%
-        text-align: right
-        float: left
+        display: flex
+        align-items: center
+
+        max-width: 85%
+        width: 100%
+        margin: auto
+        padding-top: 1em
+        flex-direction: row
+
+        ul
+            display: flex
+            padding: 0
+
+        li
+            display: inline-block
+            position: relative
+            cursor: pointer
+            vertical-align: middle
+
+            &:not(:last-of-type)
+                margin-right: 1.5em
+
+        #about
+            vertical-align: middle
+            padding-top: 1.4em
 
     .main-icon
         width: 3.5em
+        padding-left: 5%
 
 </style>
 
 <html lang="en">
     <nav>
-        <a
-            id="logo"
-            href="{base}/">
-            <img
-                class="main-icon"
-                src="{logoSrc}"
-                alt="Illustration of an astrolabe, a medieval instrument that was once used to 
-                determine the altitude of the sun or other celestial bodies, 
-                often for navigation."
-            >
-        </a>
+        <ul>
+            <li>
+                <a
+                    id="logo"
+                    href="{base}/">
+                    <div id="main-logo">
+                        <img
+                        class="main-icon"
+                        src="{logoSrc}"
+                        alt="Illustration of an astrolabe, a medieval instrument that was once used to 
+                        determine the altitude of the sun or other celestial bodies, 
+                        often for navigation."
+                        >
+                    </div>
+                </a>
+
+            </li>
+
+            <li>
+                <div id="about">
+                    <a href="/about">/about</a>
+                </div>
+            </li>
+        </ul>
     </nav>
 </html>
