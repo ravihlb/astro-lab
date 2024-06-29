395d8411dc53ed69f6c0c359405d9cc08a5a9885
ravihlb <ravihlb@protonmail.com>
2024-06-29 17:36:31 -0300

    feat: font! Fantasque Sans Mono

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
-    import Navbar from '../components/shared/Navbar.svelte'
+    import { onMount } from 'svelte'
+    import Navbar from '@components/shared/Navbar.svelte'
+    import ColorSchemeManager from '@/src/utils/ColorSchemeManager'
+
+    onMount(async () => {
+        ColorSchemeManager.initializeThemeStorage()
+    })
+
 </script>
 
 <html lang="en">
