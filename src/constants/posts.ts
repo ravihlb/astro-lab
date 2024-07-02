3d35771a0dd161db4444f079468aae7b22d674ca
ravihlb <ravihlb@protonmail.com>
2024-07-02 05:09:40 -0300

    fix(constants/posts): remove invalid import

--- src/constants/posts.ts
+++ src/constants/posts.ts
@@ -1,11 +1,9 @@
-import id2 from '@/src/routes/posts/how-to-store-playwright-test-results-on-google-cloud/props'
 import id1 from '@/src/routes/posts/neovim-filetypes-lsp/props'
 import id0 from '@/src/routes/posts/from-bloated-windows-to-arch-linux/props'
 
 import type { PostProperties } from '@/src/shared/types/PostProperties'
 
 const posts: PostProperties[] = [
-  id2,
   id1,
   id0,
 ]
