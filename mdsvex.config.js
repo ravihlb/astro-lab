244c1253869e734e46f7a0cffb8e92a0b4130bab
ravihlb <ravihlb@protonmail.com>
2024-07-05 04:39:08 -0300

    refactor(mdsvex): change mdsvex file extensions to md

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,7 +1,7 @@
 import { defineMDSveXConfig as defineConfig } from "mdsvex";
 
 const config = defineConfig({
-    extensions: [".svelte.md", ".md", ".svx"],
+    extensions: [".md", ".svx"],
 
     smartypants: {
         dashes: "oldschool",
